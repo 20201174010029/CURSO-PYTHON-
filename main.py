2fb8e4ba1f668e182f3def0ebfa52b0fafbe1ac8
JOAO DEYBSON <99667175+20201174010029@users.noreply.github.com>
2022-03-29 11:10:51 +0000

    por enquanto tá normal ..

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 # primeira 
 print('olá mundo')
+
+#segunda 
+n1 = str(input('digite seu nome: '))
+
+print('boas-vindas' ,n1,'!')
+
+#terceira
+deby1= int(input('venha me diga um numero! '))
+deby2= int(input('diga outro pfvr! '))
+
+som = deby1 + deby2 
+
+print('sua soma e igual a' ,som,'!')
