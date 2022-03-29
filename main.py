8ff1b5907e67e6e2b182e3865601f45000dfed42
JOAO DEYBSON <99667175+20201174010029@users.noreply.github.com>
2022-03-29 11:31:53 +0000

    bem normal até agora...

--- main.py
+++ main.py
@@ -1,15 +1,72 @@
-# primeira 
+# 001 
 print('olá mundo')
 
-#segunda 
+#002 
 n1 = str(input('digite seu nome: '))
 
 print('boas-vindas' ,n1,'!')
 
-#terceira
+#003
 deby1= int(input('venha me diga um numero! '))
 deby2= int(input('diga outro pfvr! '))
 
 som = deby1 + deby2 
 
 print('sua soma e igual a' ,som,'!')
+
+#004
+a= input('digite algo:  ')
+print('tipo primitivo é ',type(a))
+
+print('olá sensei!')
+
+#005
+n = int(input('digite um numero! '))
+a = n -1
+s = n +1
+
+print('analisando o valor {} seu antecessor é {} e o seu sucessor é {}'.format(n, a, s))
+
+#006
+n = int(input('digite um numero pfvr! '))
+d= n*2
+t=n*3
+r=n**(1/2)
+print('o dobro de {} vale {}.'.format(n ,d))
+print('o tiplo de {} vale {}.'.format(n, t))
+print('raiz quadrada de {} vale {}.'.format(n, r))
+
+#007
+
+a1= float(input('digite a primeira nota do aluno! '))
+a2= float(input('digite segunda nota do aluno! '))
+media= (a1 + a2)/2
+print('a media entre {} é {} é igual a {}...'.format(a1 ,a2 ,media))
+
+#008
+medida =float(input('distancia em metro: '))
+cm= medida*100
+mm= medida*1000
+print('a media em {}m corresponde a {}cm e {}mm.'.format(medida ,cm ,mm))
+
+#009
+
+abc= int(input('digite um numero pra ver sua tabuada: '))
+print('{} x {} ={}'.format(abc, 1, abc*1))
+print('{} x {} ={}'.format(abc, 2, abc*2))
+print('{} x {} ={}'.format(abc, 3, abc*3))
+print('{} x {} ={}'.format(abc, 4, abc*4))
+print('{} x {} ={}'.format(abc, 5, abc*5))
+print('{} x {} ={}'.format(abc, 6, abc*6))
+print('{} x {} ={}'.format(abc, 7, abc*7))
+print('{} x {} ={}'.format(abc, 8, abc*8))
+print('{} x {} ={}'.format(abc, 9, abc*9))
+print('{} x {} ={}'.format(abc, 10, abc*10))
+
+print('vlwzin!')
+#010
+real = float(input('quanto dinheiro você tem na carteira R$'))
+dolar = real/1.00
+print('com {} R$ você pode comprar {} US$'.format(real, dolar))
+
+#011
